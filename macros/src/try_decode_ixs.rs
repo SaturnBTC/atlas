07353748418558a4a7797df355c305ce943b6f74
Nino Borović <ninoborovic.nb@gmail.com>
2025-10-06 17:54:15 +0200

    implement custom borsh_derive_internal crate and fix borsh version mismatch with arch deps

--- macros/src/try_decode_ixs.rs
+++ macros/src/try_decode_ixs.rs
@@ -76,10 +76,10 @@
 #[macro_export]
 macro_rules! try_decode_instructions {
     ($instruction:expr, $($variant:path => $ty:ty),* $(,)?) => {{
-        use atlas_core::deserialize::AtlasDeserialize;
+        use atlas_arch::deserialize::AtlasDeserialize;
         $(
             if let Some(decoded_instruction) = <$ty>::deserialize($instruction.data.as_slice()) {
-                Some(atlas_core::instruction::DecodedInstruction {
+                Some(atlas_arch::instruction::DecodedInstruction {
                     program_id: $instruction.program_id,
                     accounts: $instruction.accounts.clone(),
                     data: $variant(decoded_instruction),
